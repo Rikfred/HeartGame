a4caa7daca0326f38224e857eb14a44122e6d245
Rikfred <111575446+Rikfred@users.noreply.github.com>
2022-08-19 14:04:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < liv; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+let liv = 0
+liv = 4
+basic.forever(function () {
+	
+})
