beaf543931a0ced82749b37b8f1a7934a9187a45
Rikfred <111575446+Rikfred@users.noreply.github.com>
2022-08-21 01:05:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,53 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < liv; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.clearScreen()
-        basic.pause(200)
-    }
-})
-input.onGesture(Gesture.FreeFall, function () {
     if (liv > 0) {
-        liv += -1
-    }
-    if (liv <= 0) {
-        basic.showIcon(IconNames.Skull)
-    } else {
-        basic.showIcon(IconNames.No)
+        paus = 0
+        for (let index = 0; index < liv; index++) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Heart)
+            basic.clearScreen()
+            basic.pause(400)
+        }
+        basic.pause(1000)
+        basic.showIcon(IconNames.SmallDiamond)
     }
 })
-input.onSound(DetectedSound.Loud, function () {
-    liv = 4
+input.onButtonPressed(Button.B, function () {
+    paus = 1
+    liv = 3
+    while (paus == 1) {
+        if (paus == 1) {
+            basic.showIcon(IconNames.Chessboard)
+            if (paus == 1) {
+                basic.showIcon(IconNames.Square)
+            }
+        }
+    }
 })
+input.onGesture(Gesture.Shake, function () {
+    if (paus == 0) {
+        paus = 1
+        if (liv > 0) {
+            liv += -1
+        }
+        if (liv == 0) {
+            basic.showIcon(IconNames.Skull)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
+let paus = 0
 let liv = 0
 liv = 3
-let paus = 1
+paus = 1
+while (paus == 1) {
+    if (paus == 1) {
+        basic.showIcon(IconNames.Chessboard)
+        if (paus == 1) {
+            basic.showIcon(IconNames.Square)
+        }
+    }
+}
 basic.forever(function () {
 	
 })
